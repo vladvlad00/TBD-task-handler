55cb9ce59dfbb9d05c6b90423a852ba868e461cf
JohnBordea <49273333+JohnBordea@users.noreply.github.com>
2021-04-18 10:06:05 +0300

    Eureca Client

--- src/main/java/ro/uaic/info/taskhandler/TaskHandlerApplication.java
+++ src/main/java/ro/uaic/info/taskhandler/TaskHandlerApplication.java
@@ -2,8 +2,10 @@ package ro.uaic.info.taskhandler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TaskHandlerApplication {
 
 	public static void main(String[] args) {
