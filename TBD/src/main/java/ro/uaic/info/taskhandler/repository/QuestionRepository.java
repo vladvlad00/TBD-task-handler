3a3a7c1e994958b526d0babc48e891cf362de182
Vlad-Enia <58520544+Vlad-Enia@users.noreply.github.com>
2021-04-19 16:45:31 +0300

    Delete TBD directory

--- TBD/src/main/java/ro/uaic/info/taskhandler/repository/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package ro.uaic.info.taskhandler.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ro.uaic.info.taskhandler.entity.Question;
-
-public interface QuestionRepository extends CrudRepository<Question, Integer>
-{
-}
--- TBD/src/main/java/ro/uaic/info/taskhandler/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package ro.uaic.info.taskhandler.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ro.uaic.info.taskhandler.entity.Task;
-
-public interface TaskRepository extends CrudRepository<Task, Integer>
-{
-}
