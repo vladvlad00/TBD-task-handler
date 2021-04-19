fd46248cfc86d52e65f2aa93e30808466909eb0d
Vlad-Enia <58520544+Vlad-Enia@users.noreply.github.com>
2021-04-19 16:45:31 +0300

    Delete TBD directory

--- TBD/src/main/java/ro/uaic/info/taskhandler/repository/ProfessorRepository.java
+++
@@ -1,8 +0,0 @@
-package ro.uaic.info.taskhandler.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ro.uaic.info.taskhandler.entity.Professor;
-
-public interface ProfessorRepository extends CrudRepository<Professor, Integer>
-{
-}
