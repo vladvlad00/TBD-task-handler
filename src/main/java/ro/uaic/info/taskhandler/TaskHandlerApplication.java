de1a9f756a1df3b38180872901a963d475798bf4
vladvlad00 <teodorescu.vlad@yahoo.com>
2021-04-25 17:25:05 +0300

    update import

--- src/main/java/ro/uaic/info/taskhandler/TaskHandlerApplication.java
+++ src/main/java/ro/uaic/info/taskhandler/TaskHandlerApplication.java
@@ -2,7 +2,7 @@ package ro.uaic.info.taskhandler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
