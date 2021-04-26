f998477ff33e5973bbe2ed9e9b292d35fbf5702f
vladvlad00 <teodorescu.vlad@yahoo.com>
2021-04-26 23:20:20 +0300

    pls

--- src/main/java/ro/uaic/info/taskhandler/TaskHandlerApplication.java
+++ src/main/java/ro/uaic/info/taskhandler/TaskHandlerApplication.java
@@ -3,7 +3,10 @@ package ro.uaic.info.taskhandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 @SpringBootApplication
 @EnableEurekaClient
 public class TaskHandlerApplication {
