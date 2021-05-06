4cb5fc27bf060adf7f93aba5e01c9f8b2ae645ac
vladvlad00 <teodorescu.vlad@yahoo.com>
2021-05-06 19:30:06 +0300

    change path to naming convention

--- src/main/java/ro/uaic/info/taskhandler/controller/ScoreAnswerController.java
+++ src/main/java/ro/uaic/info/taskhandler/controller/ScoreAnswerController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path="/scoreAnswer")
+@RequestMapping(path="/score_answer")
 public class ScoreAnswerController
 {
     @Autowired
